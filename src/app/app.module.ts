a19138dadfe44a526375d3b7f8dfa22c80f31249
Yago López <ylopez@nalej.com>
2018-09-14 14:23:19 +0200

    NP-52_Initial_Skeleton_Add_Material_Design_Support_and_Others
    - Add Material Design support
    - Add sass/scss config
    - Minor refactors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {MatButtonModule} from '@angular/material';
+
 
 import { AppComponent } from './app.component';
 
@@ -8,8 +12,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
+  exports: [MatButtonModule],
   providers: [],
   bootstrap: [AppComponent]
 })
