96aa0d9591bfde8ae3955d2d9f54c61a92a8f0ac
ivan-daishogroup <ivan@daisho.group>
2018-09-19 15:23:09 +0200

    Generated new debug-panel component

---
+++ src/app/debug-panel/debug-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-debug-panel',
+  templateUrl: './debug-panel.component.html',
+  styleUrls: ['./debug-panel.component.scss']
+})
+export class DebugPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
