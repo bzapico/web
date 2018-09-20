1074e00a7a9a0c38afa9022e61fcfb896bc88f76
Blanca <blanca@daisho.group>
2018-09-20 10:33:08 +0200

    Updated http-error messages

--- src/app/services/error-handler.service.ts
+++ src/app/services/error-handler.service.ts
@@ -14,15 +14,39 @@ export class ErrorHandlerService implements ErrorHandler {
   */
   handleError(error: HttpErrorResponse): void {
     switch (error.status) {
-        case 400 : console.log('Bad Request');
+        case 200 : console.log('OK, the request has succeeded');
             break;
-        case 401 : console.log('Unauthorized: the user does not have the necessary credentials');
+        case 201 : console.log('Created, the request has been fulfilled');
             break;
-        case 403 : console.log('Forbidden');
+        case 301 : console.log('Moved Permanently, the requests should be directed to the given URL');
             break;
-        case 404 : console.log('Not found');
+        case 400 : console.log('Bad Request, that server could not understand the request due to invalid syntax');
             break;
-        case 500 : console.log('Internal Server Error');
+        case 401 : console.log('Unauthorized, the user does not have the necessary credentials');
+            break;
+        case 403 : console.log('Forbidden, the user does not have access rights to the content');
+            break;
+        case 404 : console.log('Not found, the server can not find requested resource');
+            break;
+        case 409 : console.log('Confict, the request could not be completed due to a conflict with the current state of the resource');
+            break;
+        case 410 : console.log('Gone, the page is no longer available from the server and no forwarding address has been set up');
+            break;
+        // tslint:disable-next-line:max-line-length
+        case 500 : console.log('Internal Server Error, the server encountered an unexpected condition which prevented it from fulfilling the request');
+            break;
+        case 501 : console.log('Not Implemented, the request method is not supported by the server and cannot be handled');
+            break;
+        case 502 : console.log('Bad Gateaway, the server received an invalid response while acting as a gateway or proxy server');
+            break;
+        case 503 :
+            console.log('Service Unavailable, the server is currently unavailable');
+            break;
+        case 504 :
+            console.log('Gateway Time-Out, there is a server-server communication problem');
+            break;
+        case 511 :
+            console.log('Network Authentication Required, the user needs to authenticate to gain network access');
             break;
         default :
             console.log(error.message);
