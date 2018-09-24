e8133d8306bdc4da09cf64dc5fbf5db1e6579dd6
Blanca <blanca@daisho.group>
2018-09-24 11:26:17 +0200

    created notifications component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,26 @@ import { AppComponent } from './app.component';
 import { DebugPanelComponent } from './debug-panel/debug-panel.component';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { ButtonsModule } from 'ngx-bootstrap';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { AlertModule } from 'ngx-bootstrap/alert';
 import { FormsModule } from '@angular/forms';
+import { NotificationsComponent } from './notifications/notifications.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    DebugPanelComponent
+    DebugPanelComponent,
+    NotificationsComponent
   ],
   entryComponents: [DebugPanelComponent],
   imports: [
     FormsModule,
     BrowserModule,
     ModalModule.forRoot(),
-    ButtonsModule.forRoot()
+    ButtonsModule.forRoot(),
+    BsDropdownModule.forRoot(),
+    AlertModule.forRoot()
   ],
   providers: [{provide: ErrorHandler, useClass: ErrorHandlerService}],
   bootstrap: [AppComponent]
