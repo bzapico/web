d264e859af932d40a47a3e4cf964a705a5acf5a7
Blanca <blanca@daisho.group>
2018-10-01 10:26:43 +0200

    Added component to the test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,14 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { BsModalService, ModalModule } from 'ngx-bootstrap';
+import { MainComponent } from './main/main.component';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        MainComponent
       ],
       imports: [ModalModule.forRoot()],
       providers: [BsModalService]
