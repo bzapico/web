c5175626115afaa43389a4d6617b2b0df841253e
ivan-daishogroup <ivan@daisho.group>
2018-10-04 18:27:44 +0200

    Modified sidebar to request profile information as expected from mockupBackendService

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Backend } from '../definitions/interfaces/backend';
+import { BackendService } from '../services/backend.service';
+import { MockupBackendService } from '../services/mockup-backend.service';
+import { LocalStorageKeys } from '../definitions/const/local-storage-keys';
 
 @Component({
   // tslint:disable-next-line:component-selector
@@ -7,10 +11,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
-
-  constructor() { }
+  backend: Backend;
+  name: string;
+  role: string;
+  email: string;
+  constructor(
+    backendService: BackendService,
+    mockupBackendService: MockupBackendService
+  ) {
+    const mock = localStorage.getItem(LocalStorageKeys.sidebarMock) || null;
+    // check which backend is required (fake or real)
+    if (mock && mock === 'true') {
+      this.backend = mockupBackendService;
+    } else {
+      this.backend = backendService;
+    }
+    this.name = 'Loading ...'; // Default initialization
+    this.role = 'Loading ...'; // Default initialization
+    this.email = 'Loading ...'; // Default initialization
+   }
 
   ngOnInit() {
+    const jwtData = localStorage.getItem(LocalStorageKeys.jwtData) || null;
+    if (jwtData !== null) {
+      const userId = JSON.parse(jwtData).userId;
+      if (userId !== null) {
+        this.backend.getUserProfileInfo(userId)
+          .subscribe(response => {
+            if (response && response._body) {
+              const data = JSON.parse(response._body);
+              this.name = data.name;
+              this.email = data.email;
+              this.role = data.role;
+            }
+          });
+      }
+    }
   }
-
 }
--- src/app/utils/mocks.ts
+++ src/app/utils/mocks.ts
@@ -3,3 +3,9 @@ export const mockJwtToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9' +
 // tslint:disable-next-line:max-line-length
 '.eyJVc2VySUQiOiIxMjM0NTY3ODkwIiwiUHJpbWl0aXZlcyI6WyIwIiwiMSIsIjIiLCIzIl0sIlJvbGVuYW1lIjoiT3duZXIiLCJFeHBpcmVzQXQiOjEyMzM4NTcyNzEyNzQzfQ' +
 '.RcoS3D2fr9ofJFzPqX9DDWp-UWoOZQjg0x9uYn2D1p8';
+
+export const mockUserProfileInfo = {
+    name: 'John Doe',
+    email: 'john.doe@mail.com',
+    role: 'Owner'
+};
