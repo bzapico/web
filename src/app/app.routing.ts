3f399a4d0a54672d683f3f511f87f7603548a5af
Blanca <blanca@daisho.group>
2018-10-04 10:02:40 +0200

    Organization added to routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -6,17 +6,21 @@ import { Routes, RouterModule, provideRoutes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { MainComponent } from './main/main.component';
 import { AuthGuard } from './guards/auth.guard';
+import { OrganizationComponent } from './organization/organization.component';
 
 const appRoutes: Routes = [
     {
         path: '',
         component: MainComponent,
         canActivate: [AuthGuard]
-
     },
     {
         path: 'login',
         component: LoginComponent
+    },
+    {
+        path: 'organization',
+        component: OrganizationComponent
     },
     // otherwise redirect to home
     {
