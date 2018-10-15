65640f6cdc1019e0dc0e0cca2677878f5b4414f5
ivan-daishogroup <ivan@daisho.group>
2018-10-15 16:51:44 +0200

    Fixed build issues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,7 @@ export class AppComponent implements OnInit {
 
   constructor(
     private modalService: BsModalService,
-    private notificationsService: NotificationsService
+    public notificationsService: NotificationsService
     ) {
 
   }
