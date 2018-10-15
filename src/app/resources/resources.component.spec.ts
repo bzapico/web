3c2b6ac75b35a5d1fd5600cc5fb6a5a3d82f2afd
Blanca <blanca@daisho.group>
2018-10-15 17:11:27 +0200

    Tests fixed

--- src/app/resources/resources.component.spec.ts
+++ src/app/resources/resources.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { ResourcesComponent } from './resources.component';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('ResourcesComponent', () => {
   let component: ResourcesComponent;
@@ -8,7 +10,8 @@ describe('ResourcesComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ResourcesComponent ]
+      declarations: [ ResourcesComponent ],
+      imports: [ NgxChartsModule, CarouselModule, BrowserAnimationsModule ]
     })
     .compileComponents();
   }));
