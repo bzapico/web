a951c6346c4a13f2ed8e59b2e428f285a125a607
Blanca <blanca@daisho.group>
2018-11-08 15:32:27 +0100

    Application component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,6 +27,7 @@ import { EditClusterComponent } from './edit-cluster/edit-cluster.component';
 import { ResourcesComponent } from './resources/resources.component';
 import { AddUserComponent } from './add-user/add-user.component';
 import { EditUserComponent } from './edit-user/edit-user.component';
+import { ApplicationsComponent } from './applications/applications.component';
 
 
 @NgModule({
@@ -43,6 +44,7 @@ import { EditUserComponent } from './edit-user/edit-user.component';
     AddUserComponent,
     EditClusterComponent,
     EditUserComponent,
+    ApplicationsComponent,
   ],
   entryComponents: [
     DebugPanelComponent,
