2435b908f3ed3fc2de600effc4342724e0fdb033
ivan-daishogroup <ivan@daisho.group>
2018-11-08 19:30:56 +0100

    Initial steps to integrate real backend

--- src/app/services/backend.service.ts
+++ src/app/services/backend.service.ts
@@ -1,6 +1,9 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
 import { Backend } from '../definitions/interfaces/backend';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { DOCUMENT } from '@angular/common';
+import { LocalStorageKeys } from '../definitions/const/local-storage-keys';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +11,29 @@ import { Observable } from 'rxjs';
 
 export class BackendService implements Backend {
 
-  constructor() { }
+  urlBase: string;
+  urlAPI: string;
+  
+
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private http: HttpBasicAuthService) {
+    let portAPI: number;
+    if (environment.production) {
+      this.urlBase = this.replaceUrlPort(this.document.location.href, portAPI);
+      this.urlAPI = this.urlBase + 'api/v0/';
+    } else {
+      let tmpUrl = localStorage.getItem(LocalStorageKeys.urlBase);
+      if (tmpUrl != null && tmpUrl.indexOf('https') >= 0) {
+        this.urlBase = tmpUrl;
+        this.urlAPI = this.urlBase + 'api/v0/';
+      } else {
+        this.urlBase = 'https://172.31.3.99:30000/'; // connected to FMX (required vpn)
+        this.urlAPI = this.urlBase + 'api/v0/';
+        localStorage.setItem(LocalStorageKeys.urlBase, this.urlBase);
+      }
+   }
+  }
 
   login(email: string, password: string): Observable<any> {
     throw new Error('Method not implemented.');
