a46aeef06c9c05bf8abc8ad19ffd0b09cb005233
Blanca <blanca@daisho.group>
2018-11-13 18:01:57 +0100

    Apps info component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,6 +27,7 @@ import { EditClusterComponent } from './edit-cluster/edit-cluster.component';
 import { ResourcesComponent } from './resources/resources.component';
 import { AddUserComponent } from './add-user/add-user.component';
 import { EditUserComponent } from './edit-user/edit-user.component';
+import { AppsInfoComponent } from './apps-info/apps-info.component';
 
 
 @NgModule({
@@ -43,6 +44,7 @@ import { EditUserComponent } from './edit-user/edit-user.component';
     AddUserComponent,
     EditClusterComponent,
     EditUserComponent,
+    AppsInfoComponent,
   ],
   entryComponents: [
     DebugPanelComponent,
---
+++ src/app/apps-info/apps-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-apps-info',
+  templateUrl: './apps-info.component.html',
+  styleUrls: ['./apps-info.component.scss']
+})
+export class AppsInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
