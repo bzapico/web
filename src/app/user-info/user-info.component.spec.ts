d459034300375de8d062c1009ef0d6b591e2aba8
Blanca <blanca@daisho.group>
2018-11-26 17:44:44 +0100

    Added tests to user info component

--- src/app/user-info/user-info.component.spec.ts
+++ src/app/user-info/user-info.component.spec.ts
@@ -27,7 +27,35 @@ describe('UserInfoComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('checkUserRole() - Should check the role of current user', () => {
+    const checkUserRole = spyOn(component, 'checkUserRole').and.returnValue(true);
+    const buttonRole = {};
+
+    checkUserRole(buttonRole);
+
+    expect(checkUserRole).toHaveBeenCalledWith(buttonRole);
   });
+
+  it('deleteUser() - Should test if the method is called with parameter as mock user object', () => {
+    const mockUserObj = {
+      userId: 'john@domain.com',
+      name: 'John Doe',
+      roles: ['operator']
+    };
+    const deleteUser = spyOn(component, 'deleteUser').and.returnValue(true);
+
+    deleteUser(mockUserObj);
+
+    expect(deleteUser).toHaveBeenCalledWith(mockUserObj);
+  });
+
+  it('resetPassword() - Should check the role of current user', () => {
+    const resetPassword = spyOn(component, 'resetPassword').and.returnValue(true);
+    const password = {};
+
+    resetPassword(password);
+
+    expect(resetPassword).toHaveBeenCalledWith(password);
+  });
+
 });
