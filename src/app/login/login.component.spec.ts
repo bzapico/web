5a391751a72299d60a9af472cbc7fed2668dbf90
Blanca <blanca@daisho.group>
2018-11-27 12:18:24 +0100

    Added tests to login

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,15 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AuthService } from '../services/auth.service';
 import { RouterTestingModule } from '@angular/router/testing';
 import { BsModalService, ModalModule } from 'ngx-bootstrap';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let de: DebugElement;
+  let el: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -18,7 +22,8 @@ describe('LoginComponent', () => {
         ModalModule.forRoot(),
         HttpClientTestingModule,
         ReactiveFormsModule,
-        RouterTestingModule
+        RouterTestingModule,
+        FormsModule
       ],
       providers: [
         AuthService,
@@ -31,7 +36,27 @@ describe('LoginComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
+
     fixture.detectChanges();
+
+    de = fixture.debugElement.query(By.css('form'));
+    el = de.nativeElement;
+  });
+
+  it('onSubmit() - It should be submitted to true', async() => {
+    component.onSubmit();
+
+    expect(component.loginRequest).toBeTruthy();
+  });
+
+  // tslint:disable-next-line:max-line-length
+  it('onSubmit() - Should triggered when clicking the login button and calls the login function on the auth service to check the credentials', async() => {
+    fixture.detectChanges();
+    spyOn(component, 'onSubmit');
+    el = fixture.debugElement.query(By.css('button')).nativeElement;
+    el.click();
+
+    expect(component.onSubmit).toHaveBeenCalledTimes(0);
   });
 
   it('Form shoud be invalid', async() => {
