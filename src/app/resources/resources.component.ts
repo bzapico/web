250deff171e7dccf220858f4b6e9260f194460ea
Blanca <blanca@daisho.group>
2019-01-11 13:25:02 +0100

    Added same styles to applications time line

--- src/app/resources/resources.component.ts
+++ src/app/resources/resources.component.ts
@@ -80,7 +80,7 @@ export class ResourcesComponent implements OnInit, OnDestroy {
   /**
    * Refresh ratio
    */
-  REFRESH_INTERVAL = 20000;
+  REFRESH_INTERVAL = 20000; // 20 seconds
 
   /**
    * Hold request error message or undefined
