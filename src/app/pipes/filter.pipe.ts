f273236e4dc06a5c904a21ee0bf7ccfe4c30e25f
Blanca <blanca@daisho.group>
2019-01-23 16:15:33 +0100

    Added functionality to filter pipe

--- src/app/pipes/filter.pipe.ts
+++ src/app/pipes/filter.pipe.ts
@@ -1,12 +1,21 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { Pipe, PipeTransform, Injectable } from '@angular/core';
 
 @Pipe({
   name: 'filter'
 })
+@Injectable()
 export class FilterPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
-  }
+  transform(items: any[], field: string, value: string): any[] {
+    if (!items) {
+      return [];
+    }
+    if (!field || !value) {
+      return items;
+    }
 
+    return items.filter(singleItem =>
+      singleItem[field].toLowerCase().includes(value.toLowerCase())
+    );
+  }
 }
