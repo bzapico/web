9c855ff85d3e9c4ceac1411139ec6d8ca686480f
Blanca <blanca@daisho.group>
2019-01-28 11:50:11 +0100

    Filter pipe improved

--- src/app/pipes/filter.pipe.ts
+++ src/app/pipes/filter.pipe.ts
@@ -6,16 +6,36 @@ import { Pipe, PipeTransform, Injectable } from '@angular/core';
 @Injectable()
 export class FilterPipe implements PipeTransform {
 
-  transform(items: any[], field: string, value: string): any[] {
-    if (!items) {
-      return [];
-    }
-    if (!field || !value) {
-      return items;
-    }
+  /**
+   *
+   * @param items List of items to filter
+   * @param term  a string term to compare with every property of the list
+   *
+   */
+  static filter(items: Array<{ [key: string]: any }>, term: string): Array<{ [key: string]: any }> {
 
-    return items.filter(singleItem =>
-      singleItem[field].toLowerCase().includes(value.toLowerCase())
-    );
+    const toCompare = term.toLowerCase();
+
+    return items.filter(function (item: any) {
+      for (const property in item) {
+        if (item[property] === null || item[property] === undefined) {
+          continue;
+        }
+        if (item[property].toString().toLowerCase().includes(toCompare)) {
+          return true;
+        }
+      }
+      return false;
+    });
+  }
+
+  /**
+   * @param items object from array
+   * @param term term's search
+   */
+  transform(items: any, term: string): any {
+    if (!term || !items) { return items; }
+
+    return FilterPipe.filter(items, term);
   }
 }
