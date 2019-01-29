832e1ec81e74a09a6926a0736690ba270c2320fa
Blanca <blanca@daisho.group>
2019-01-29 17:27:44 +0100

    Added provisional logic

--- src/app/devices/devices.component.ts
+++ src/app/devices/devices.component.ts
@@ -1,15 +1,312 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap';
+import { Backend } from '../definitions/interfaces/backend';
+import { BackendService } from '../services/backend.service';
+import { MockupBackendService } from '../services/mockup-backend.service';
+import { NotificationsService } from '../services/notifications.service';
+import { LocalStorageKeys } from '../definitions/const/local-storage-keys';
+import { mockAppChart, mockAppPieChart } from '../utils/mocks';
 
 @Component({
   selector: 'app-devices',
   templateUrl: './devices.component.html',
   styleUrls: ['./devices.component.scss']
 })
-export class DevicesComponent implements OnInit {
+export class DevicesComponent implements OnInit, OnDestroy  {
+  /**
+   * Backend reference
+   */
+  backend: Backend;
 
-  constructor() { }
+  /**
+   * Model that hold organization ID
+   */
+  organizationId: string;
+
+  /**
+   * Loaded Data status
+   */
+  loadedData: boolean;
+
+  /**
+   * List of available apps instances
+   */
+  instances: any[];
+
+  /**
+   * List of registered apps
+   */
+  registered: any[];
+
+  labels: any[];
+
+  /**
+   * Number of running instances
+   */
+  countRunning: number;
+
+  /**
+   * Number of registered apps
+   */
+  countRegistered: number;
+
+  /**
+   * Interval reference
+   */
+  refreshIntervalRef: any;
+
+  /**
+   * Refresh ratio reference
+   */
+  REFRESH_RATIO = 20000; // 20 seconds
+
+  /**
+   * Charts references
+   */
+  mockAppChart: any;
+  mockAppPieChart: any;
+  appsChart: any;
+
+  /**
+   * Reference for the service that allows the user info component
+   */
+  modalRef: BsModalRef;
+
+  /**
+   * Hold request error message or undefined
+   */
+  requestError: string;
+
+  /**
+   * Pie Chart options
+   */
+  gradient = true;
+  doughnut = true;
+  colorScheme = {
+    domain: ['#0937FF', '#949494']
+  };
+  customColors = [
+    {
+      name: 'Running',
+      value: '#0000ff'
+    },
+    {
+      name: 'error',
+      value: '#00ff00'
+    }
+  ];
+
+  /**
+   * Line Chart options
+   */
+  showXAxis = true;
+  showYAxis = true;
+  showXAxisLabel = false;
+  showYAxisLabel = false;
+  showGridLines = true;
+  showRefLines = true;
+  showRefLabels = false;
+  schemeType = 'ordinal';
+  rangeFillOpacity = 0.0;
+  referenceLines = [
+    {
+      name: 'xline',
+      value: 0
+    }
+  ];
+
+  /**
+   * NGX-Charts object-assign required object references (for rendering)
+   */
+  instancesTimelineChart: any;
+  instancesPieChart: any;
+
+  constructor(
+    private modalService: BsModalService,
+    private backendService: BackendService,
+    private mockupBackendService: MockupBackendService,
+    private notificationsService: NotificationsService
+  ) {
+    const mock = localStorage.getItem(LocalStorageKeys.appsMock) || null;
+    // Check which backend is required (fake or real)
+    if (mock && mock === 'true') {
+      this.backend = mockupBackendService;
+    } else {
+      this.backend = backendService;
+    }
+    // Default initialization
+    this.instances = [];
+    this.registered = [];
+    this.labels = [];
+    this.countRegistered = 0;
+    this.loadedData = false;
+    this.appsChart = [{name: 'Running apps %', series: []}];
+    this.requestError = '';
+    /**
+     * Charts reference init
+     */
+    Object.assign(this, {mockAppChart, mockAppPieChart});
+
+   }
 
   ngOnInit() {
+        // Get User data from localStorage
+        const jwtData = localStorage.getItem(LocalStorageKeys.jwtData) || null;
+        if (jwtData !== null) {
+          this.organizationId = JSON.parse(jwtData).organizationID;
+            this.updateAppInstances(this.organizationId);
+            this.updateRegisteredInstances(this.organizationId);
+            this.refreshIntervalRef = setInterval(() => {
+              this.updateAppInstances(this.organizationId);
+              this.updateRegisteredInstances(this.organizationId);
+            }, this.REFRESH_RATIO); // Refresh each 60 seconds
+        }
+    }
+  ngOnDestroy() {
+    clearInterval(this.refreshIntervalRef);
+  }
+/**
+   * Updates instances array
+   * @param organizationId Organization identifier
+   */
+  updateAppInstances(organizationId: string) {
+    if (organizationId !== null) {
+      // Request to get apps instances
+      this.backend.getInstances(this.organizationId)
+      .subscribe(response => {
+          this.instances = response.instances || [];
+          this.updatePieChartStats(this.instances);
+          this.updateRunningAppsLineChart(this.instances);
+          if (!this.loadedData) {
+            this.loadedData = true;
+          }
+      }, errorResponse => {
+        this.loadedData = true;
+        this.requestError = errorResponse.error.message;
+      });
+    }
   }
 
+  /**
+   * Updates registered apps array
+   * @param organizationId Organization identifier
+   */
+  updateRegisteredInstances(organizationId: string) {
+    if (organizationId !== null) {
+      // Request to get registered apps
+      this.backend.getRegisteredApps(this.organizationId)
+      .subscribe(response => {
+          this.registered = response.descriptors || [];
+      });
+    }
+  }
+
+
+  /**
+   * Updates pie chart status
+   * @param instances Array of instance objects
+   */
+  updatePieChartStats(instances: any[]) {
+    let running = 0;
+    if (instances) {
+      instances.forEach(app => {
+        if (app.status_name === 'RUNNING') {
+          running += 1;
+        }
+      });
+      this.countRunning = running;
+      this.instancesPieChart = this.generateSummaryChartData(this.countRunning, instances.length);
+    }
+  }
+
+  /**
+   * Updates timeline chart
+   * @param instances Instances array
+   */
+  updateRunningAppsLineChart(instances) {
+    let runningAppsCount = 0;
+    instances.forEach(instance => {
+      if (instance && instance.status_name.toLowerCase() === 'running') {
+        runningAppsCount += 1;
+      }
+    });
+
+    const now = new Date(Date.now());
+    let minutes: any = now.getMinutes();
+    let seconds: any = now.getSeconds();
+    if (minutes < 10) {
+      minutes = '0' + now.getMinutes();
+    }
+    if (seconds < 10) {
+      seconds = '0' + now.getSeconds();
+    }
+    const entry = {
+      'value': runningAppsCount / instances.length * 100,
+      'name':  now.getHours() + ':' + minutes + ':' + seconds
+    };
+
+    if (this.appsChart[0].series.length > 5) {
+      // Removes first element
+      this.appsChart[0].series.shift();
+    }
+    this.appsChart[0].series.push(entry);
+    this.appsChart = [...this.appsChart];
+  }
+
+  /**
+   * Generates the NGX-Chart required JSON object for pie chart rendering
+   * @param running Number of running nodes in a cluster
+   * @param total Number of total nodes in a cluster
+   * @returns anonym array with the required object structure for pie chart rendering
+   */
+  generateSummaryChartData(running: number, total: number): any[] {
+    return [
+      {
+        name: 'Running',
+        value: running
+      },
+      {
+        name: 'Stopped',
+        value: total - running
+      }];
+  }
+
+  /**
+   * Parse to string labels map
+   * @param labels Key-value map that contains the labels
+   */
+  labelsToString(labels: any) {
+    if (!labels || labels === '-') {
+      return ;
+    }
+    return Object.entries(labels);
+  }
+
+  /**
+   * Checks if the cluster status requires an special css class
+   * @param status Cluster status name
+   * @param className CSS class name
+   */
+  classStatusCheck(status: string, className: string): boolean {
+    switch (status.toLowerCase()) {
+      case 'running': {
+        if (className.toLowerCase() === 'running') {
+          return true;
+        }
+        break;
+      }
+      case 'error': {
+        if (className.toLowerCase() === 'error') {
+          return true;
+        }
+        break;
+      }
+     default: {
+        if (className.toLowerCase() === 'process') {
+          return true;
+        }
+        return false;
+      }
+    }
+  }
 }
