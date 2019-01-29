7d008c0c80c5fedebb4437377c262e4ede6b5412
Blanca <blanca@daisho.group>
2019-01-29 09:53:55 +0100

    Added test to filter pipe

--- src/app/pipes/filter.pipe.spec.ts
+++ src/app/pipes/filter.pipe.spec.ts
@@ -1,8 +1,27 @@
 import { FilterPipe } from './filter.pipe';
 
 describe('FilterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FilterPipe();
-    expect(pipe).toBeTruthy();
+  let pipe: FilterPipe;
+
+  beforeEach(() => {
+      pipe = new FilterPipe();
+  });
+
+  it('Return the same list if the "term" is invalid', () => {
+    const list = [{ a: 'b' }, { a: 'c' }];
+    expect(pipe.transform(list, null)).toBe(list);
+    expect(pipe.transform(list, undefined)).toBe(list);
+    expect(pipe.transform(null, undefined)).toBe(null);
+    expect(pipe.transform(undefined, undefined)).toBe(undefined);
+  });
+
+  it('Return the same list if the "list" is invalid', () => {
+      expect(pipe.transform(null, 'hello')).toBe(null);
+      expect(pipe.transform(undefined, 'hello')).toBe(undefined);
+  });
+
+  it('Filter the elements', () => {
+      const list = [{ a: 'b' }, { a: 'c' }];
+      expect(pipe.transform(list, 'b')).toEqual([{ a: 'b' }]);
   });
 });
