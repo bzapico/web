a17ec7d12b132ea127acc4cffe066b472ced0fe7
ivan-daishogroup <ivan@daisho.group>
2019-02-26 12:38:27 +0100

    Addressed issue with backend service reference

--- src/app/devices/devices.component.ts
+++ src/app/devices/devices.component.ts
@@ -174,7 +174,7 @@ export class DevicesComponent implements OnInit, OnDestroy  {
     if (mock && mock === 'true') {
       this.backend = mockupBackendService;
     } else {
-      this.backend = mockupBackendService;
+      this.backend = backendService;
     }
     // Default initialization
     this.devices = [];
