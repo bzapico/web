75d6e184bcb53abb350b0cc64ab061bdc1b298db
Blanca <blanca@daisho.group>
2019-02-28 17:20:24 +0100

    App registered extended view styles in progress

--- src/app/registered-info/registered-info.component.ts
+++ src/app/registered-info/registered-info.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { Backend } from '../definitions/interfaces/backend';
+import { BackendService } from '../services/backend.service';
+import { MockupBackendService } from '../services/mockup-backend.service';
+import { NotificationsService } from '../services/notifications.service';
+import { LocalStorageKeys } from '../definitions/const/local-storage-keys';
 
 @Component({
   selector: 'app-registered-info',
@@ -6,10 +11,263 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registered-info.component.scss']
 })
 export class RegisteredInfoComponent implements OnInit {
+  /**
+   * Backend reference
+   */
+  backend: Backend;
 
-  constructor() { }
+  /**
+   * Model that hold organization ID
+   */
+  organizationId: string;
+
+  /**
+   * Loaded Data status
+   */
+  loadedData: boolean;
+
+  /**
+   * List of available apps instances
+   */
+  instances: any[];
+
+  /**
+   * List of registered apps
+   */
+  registered: any[];
+
+  /**
+   * Models that hold the active group
+   */
+  activeGroupId: string;
+
+  /**
+   * List of available devices groups
+   */
+  groups: any[];
+
+  /**
+   * Models that keeps the displayed groups names length
+   */
+  displayedGroupsNamesLength: number;
+  maxLabelsLength: number;
+
+  /**
+   * List of active displayed group
+   */
+  displayedGroups: any[];
+
+  /**
+   * Count of num max for displayed groups
+   */
+  DISPLAYED_GROUP_MAX = 5;
+
+  /**
+   * Hold request error message or undefined
+   */
+  requestError: string;
+
+   /**
+   * Models that hold the sort info needed to sortBy pipe
+   */
+  sortedBy: string;
+  reverse: boolean;
+
+  /**
+   * Model that hold the search term in search box
+   */
+  searchTerm: string;
+
+  /**
+   * Variable to store the value of the filter search text and sortBy pipe
+   */
+  filterField: boolean;
+
+
+  constructor(
+    private backendService: BackendService,
+    private mockupBackendService: MockupBackendService,
+    private notificationsService: NotificationsService
+  ) {
+    const mock = localStorage.getItem(LocalStorageKeys.appsMock) || null;
+    // Check which backend is required (fake or real)
+    if (mock && mock === 'true') {
+      this.backend = mockupBackendService;
+    } else {
+      this.backend = backendService;
+    }
+    // Default initialization
+    this.instances = [];
+    this.registered = [];
+    this.displayedGroups = [];
+    this.activeGroupId = 'ALL';
+    this.requestError = '';
+    // SortBy
+    this.sortedBy = '';
+    this.reverse = false;
+    this.searchTerm = '';
+    // Filter field
+    this.filterField = false;
+  }
 
   ngOnInit() {
+    // Get User data from localStorage
+    const jwtData = localStorage.getItem(LocalStorageKeys.jwtData) || null;
+    if (jwtData !== null) {
+      this.organizationId = JSON.parse(jwtData).organizationID;
+        this.updateGroupsList(this.organizationId);
+
+    }
+    this.updateDisplayedGroupsNamesLength();
+  }
+
+
+  /**
+   * Calculates the number of characters needed to hide the title of tabs, breakpoints calculated through manual testing
+   * @param event to pass in onResize method
+   */
+  @HostListener('window:resize', ['$event'])
+    onResize(event) {
+      if (event.target.innerWidth < 1280) {
+        this.maxLabelsLength = 55;
+      } else if (event.target.innerWidth < 1440) {
+        this.maxLabelsLength = 65;
+      } else if (event.target.innerWidth < 1613) {
+        this.maxLabelsLength = 75;
+      } else if (event.target.innerWidth < 1920) {
+        this.maxLabelsLength = 85;
+      } else {
+        this.maxLabelsLength = 100;
+      }
+    }
+
+  /**
+   * Changes to active group
+   * @param groupId device group identifier
+   */
+  changeActiveGroup(groupId: string) {
+    this.activeGroupId = groupId;
+  }
+
+  /**
+   * Checks if the device group is active to show in the tabs
+   * @param groupId device group identifier
+   */
+  amIactive(groupId) {
+    if (groupId === this.activeGroupId) {
+      return 'active';
+    }
+    // Empty class when is not active
+    return '';
+  }
+
+  /**
+   * Checks if there are less than a maximum number groups in groups list
+   * @param groups groups list identifier
+   */
+  haveIGroups(groups) {
+    if (groups.length > this.DISPLAYED_GROUP_MAX) {
+      return '';
+    }
+    return 'opacity';
+  }
+
+  /**
+   * Sortby pipe in the component
+   * @param categoryName the name of the chosen category
+   */
+  setOrder(categoryName: string) {
+    if (this.sortedBy === categoryName) {
+      this.reverse = !this.reverse;
+      this.filterField = false;
+    }
+    this.sortedBy = categoryName;
+    this.filterField = true;
+  }
+
+  /**
+   * Reset all the filters fields
+   */
+  resetFilters() {
+    this.filterField = false;
+    this.searchTerm = '';
+    this.sortedBy = '';
+  }
+
+  /**
+   * Gets the category headers to add a class
+   * @param categoryName the class for the header category
+   */
+  getCategoryCSSClass(categoryName: string) {
+    if (this.sortedBy === '') {
+      return 'default';
+    } else {
+      if (this.sortedBy === categoryName) {
+        return 'enabled';
+      } else if (this.sortedBy !== categoryName) {
+        return 'disabled';
+      }
+    }
+  }
+
+  /**
+   * Updates the displayed groups chars length to calculate the number of letters displayed according to the size of the viewport
+   */
+  updateDisplayedGroupsNamesLength() {
+    this.displayedGroupsNamesLength = 0;
+    this.displayedGroups.forEach(group => {
+      this.displayedGroupsNamesLength += group.name.length;
+    });
+  }
+
+  /**
+   * Displayed groups list swipes left by pressing the arrow button functionality
+   */
+  swipeLeft() {
+    const index = this.groups.map(x => x.device_group_id).indexOf(this.displayedGroups[0].device_group_id);
+    if (index !== -1 && index > 0) {
+      this.displayedGroups.unshift(this.groups[index - 1]);
+      this.displayedGroups.pop();
+      this.updateDisplayedGroupsNamesLength();
+    }
+  }
+
+  /**
+   * Displayed groups list swipes right by pressing the arrow button functionality
+   */
+  swipeRight() {
+    const index = this.groups.map(x => x.device_group_id).indexOf(this.displayedGroups[this.displayedGroups.length - 1].device_group_id);
+    if (index !== -1 && this.groups[index + 1]) {
+      this.displayedGroups.push(this.groups[index + 1]);
+      this.displayedGroups.shift();
+    }
+    this.updateDisplayedGroupsNamesLength();
+  }
+
+  /**
+   * Requests an updated list of available devices group to update the current one
+   * @param organizationId Organization identifier
+   */
+  updateGroupsList(organizationId: string) {
+    if (organizationId !== null) {
+      // Requests an updated devices group list
+      this.backend.getGroups(this.organizationId)
+      .subscribe(response => {
+        this.groups = response.groups || [];
+        if (this.displayedGroups.length === 0 && this.groups.length > 0) {
+          for (let index = 0; index < this.groups.length && index < this.DISPLAYED_GROUP_MAX; index++) {
+            this.displayedGroups.push(this.groups[index]);
+          }
+        }
+        this.updateDisplayedGroupsNamesLength();
+        if (!this.loadedData) {
+          this.loadedData = true;
+        }
+      }, errorResponse => {
+          this.loadedData = true;
+          this.requestError = errorResponse.error.message;
+        });
+    }
   }
 
 }
