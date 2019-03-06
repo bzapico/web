0e07ec9a8b2fcd1871ab2276e6bba5064dc600e5
Blanca <blanca@daisho.group>
2019-03-06 11:10:10 +0100

    Fixed tests

--- src/app/registered-info/registered-info.component.spec.ts
+++ src/app/registered-info/registered-info.component.spec.ts
@@ -1,6 +1,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegisteredInfoComponent } from './registered-info.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NgxGraphModule } from '@swimlane/ngx-graph';
+import { TooltipModule } from '@swimlane/ngx-charts';
+import { ButtonsModule, ModalModule } from 'ngx-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { FilterPipe } from '../pipes/filter.pipe';
+import { SortByPipe } from '../pipes/sort-by.pipe';
+
 
 describe('RegisteredInfoComponent', () => {
   let component: RegisteredInfoComponent;
@@ -8,7 +18,21 @@ describe('RegisteredInfoComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RegisteredInfoComponent ]
+      declarations: [
+        RegisteredInfoComponent,
+        FilterPipe,
+        SortByPipe
+      ],
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule,
+        NgxGraphModule,
+        TooltipModule,
+        ButtonsModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        ModalModule.forRoot(),
+      ]
     })
     .compileComponents();
   }));
