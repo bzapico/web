e918557ae0597cf630a6728ff9a57b1f6b83d9b6
Blanca <blanca@daisho.group>
2019-03-08 12:35:24 +0100

    Styles in progress

--- src/app/instances-info/instances-info.component.ts
+++ src/app/instances-info/instances-info.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { Backend } from '../definitions/interfaces/backend';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap';
+import { BackendService } from '../services/backend.service';
+import { NotificationsService } from '../services/notifications.service';
+import { MockupBackendService } from '../services/mockup-backend.service';
+import { ActivatedRoute } from '@angular/router';
+import { LocalStorageKeys } from '../definitions/const/local-storage-keys';
+import * as shape from 'd3-shape';
+import { AddLabelComponent } from '../add-label/add-label.component';
 
 @Component({
   selector: 'app-instances-info',
@@ -7,9 +16,674 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InstancesInfoComponent implements OnInit {
 
-  constructor() { }
+  /**
+   * Backend reference
+   */
+  backend: Backend;
+
+  /**
+   * Model that hold organization ID
+   */
+  organizationId: string;
+
+  /**
+   * Loaded Data status
+   */
+  loadedData: boolean;
+
+  /**
+   * List of available apps instances
+   */
+  instances: any[];
+
+  /**
+   * Model that hold organization ID
+   */
+  instanceId: string;
+
+  /**
+   * Model that hold instance
+   */
+  instance: any;
+  enabled: boolean;
+
+  /**
+   * List of available services
+   */
+  services: any[];
+
+  /**
+   * Models that hold the active group
+   */
+  activeGroupId: string;
+
+  /**
+   * List of available services groups
+   */
+  groups: any[];
+
+  /**
+   * List of labels
+   */
+  labels: any[];
+
+  /**
+   * List of selected labels from an entity
+   */
+  selectedLabels = [];
+  entityId: boolean;
+
+  /**
+   * Models that keeps the displayed groups names length
+   */
+  displayedGroupsNamesLength: number;
+  maxLabelsLength: number;
+
+  /**
+   * List of active displayed group
+   */
+  displayedGroups: any[];
+
+  /**
+   * Count of num max for displayed groups
+   */
+  DISPLAYED_GROUP_MAX = 4;
+
+  /**
+   * Hold request error message or undefined
+   */
+  requestError: string;
+
+  /**
+   * Models that hold the sort info needed to sortBy pipe
+   */
+  sortedBy: string;
+  reverse: boolean;
+
+  /**
+   * Model that hold the search term in search box
+   */
+  searchTerm: string;
+
+  /**
+   * Variable to store the value of the filter search text and sortBy pipe
+   */
+  filterField: boolean;
+
+  /**
+   * Reference for the service that allows the modal component
+   */
+  modalRef: BsModalRef;
+
+  /**
+   *  Show the services graph tab
+   */
+  showGraph: boolean;
+
+  /**
+   * NGX-Graphs object-assign required object references (for rendering)
+   */
+  mockServicesGraph: any;
+
+  /**
+   * Graph options
+   */
+  showlegend: boolean;
+  graphData: any;
+  orientation: string;
+  curve: any;
+  autoZoom: boolean;
+  autoCenter: boolean;
+  enableZoom: boolean;
+  colorScheme: any;
+  view: any[];
+  width: number;
+  height: number;
+  draggingEnabled: boolean;
+  nalejColorScheme: string[];
+  nextColorIndex: number;
+
+  constructor(
+    private modalService: BsModalService,
+    private backendService: BackendService,
+    private mockupBackendService: MockupBackendService,
+    private notificationsService: NotificationsService,
+    private route: ActivatedRoute
+  ) {
+    const mock = localStorage.getItem(LocalStorageKeys.appsMock) || null;
+    // Check which backend is required (fake or real)
+    if (mock && mock === 'true') {
+      this.backend = mockupBackendService;
+    } else {
+      this.backend = backendService;
+    }
+    // Default initialization
+    this.instances = [];
+    this.labels = [];
+    this.displayedGroups = [];
+    this.activeGroupId = 'ALL';
+    this.requestError = '';
+    this.showGraph = false;
+    this.enabled = false;
+
+    // SortBy
+    this.sortedBy = '';
+    this.reverse = false;
+    this.searchTerm = '';
+    // Filter field
+    this.filterField = false;
+     // Graph initialization
+     this.showlegend = false;
+     this.orientation = 'TB';
+     this.curve = shape.curveBundle;
+     this.autoZoom = true;
+     this.autoCenter = true;
+     this.enableZoom = true;
+     this.draggingEnabled = false;
+    //  this.view = [605, 250];
+     this.colorScheme = {
+       domain: ['#6C86F7']
+     };
+    // TODO
+     this.graphData = {
+       nodes: [
+        {
+          id: 'start',
+          label: 'start',
+          color: '#6C86F7',
+          tooltip: 'SE: 45665498764'
+        }, {
+          id: '1',
+          label: 'Query ThreatConnect',
+          color: '#6C86F7',
+          tooltip: 'SE: 45665498764'
+        }, {
+          id: '2',
+          label: 'Query XForce',
+          color: '#6C86F7',
+          tooltip: 'SE: 45665498764'
+        }, {
+          id: '7',
+          label: 'Query ThCt',
+          color: '#6C86F7',
+          tooltip: 'SE: 45665498764'
+        }, {
+          id: '3',
+          label: 'Format Results',
+          color: '#6C86F7',
+          tooltip: 'SE: 45665498764'
+        }, {
+          id: '4',
+          label: 'Search Splunk',
+          color: '#6C86F7',
+          tooltip: 'SE: 45665498764'
+        }, {
+          id: '5',
+          label: 'Block LDAP',
+          color: '#6C86F7',
+          tooltip: 'SE: 45665498764'
+        }, {
+          id: '6',
+          label: 'Email Results',
+          color: '#6C86F7',
+          tooltip: 'SE: 45665498764'
+        }
+       ],
+       links: [
+        {
+          source: 'start',
+          target: '1',
+          label: 'links to'
+        }, {
+          source: 'start',
+          target: '2'
+        }, {
+          source: 'start',
+          target: '7'
+        }, {
+          source: '1',
+          target: '3',
+          label: 'related to'
+        }, {
+          source: '2',
+          target: '4'
+        }, {
+          source: '2',
+          target: '6'
+        }, {
+          source: '3',
+          target: '5'
+        }
+       ]
+     };
+
+     this.nalejColorScheme = [
+       '#1725AE',
+       '#040D5A',
+       '#0F1B8C',
+       '#01073A',
+       '#091374'
+     ];
+     this.nextColorIndex = 0;
+
+  }
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.instanceId = params['instanceId']; // (+) converts string 'id' to a number
+   });
+    // Get User data from localStorage
+    const jwtData = localStorage.getItem(LocalStorageKeys.jwtData) || null;
+    if (jwtData !== null) {
+      this.organizationId = JSON.parse(jwtData).organizationID;
+        if (this.organizationId !== null) {
+          this.updateGroupsList(this.organizationId);
+          this.updateAppInstances(this.organizationId);
+        }
+        this.updateDisplayedGroupsNamesLength();
+    }
+    this.backend.getAppInstance(this.organizationId,  this.instanceId)
+    .subscribe(instance => {
+      console.log('instance ', instance);
+        this.instance = instance;
+        this.toGraphData(instance);
+        if (!this.loadedData) {
+          this.loadedData = true;
+        }
+    });
   }
+
+
+  /**
+   * Transforms the data needed to create the grapho
+   * @param instance instance object
+   */
+  toGraphData(instance) {
+    instance.groups.forEach(group => {
+      const nodeGroup = {
+        id: group.service_group_instance_id,
+        label: group.name,
+        tooltip: 'GROUP: ' + group.service_group_instance_id,
+        color: this.nalejColorScheme[this.nextColorIndex],
+        group: group.service_group_instance_id
+      };
+      this.graphData.nodes.push(nodeGroup);
+      group.service_instances.forEach(service => {
+        const nodeService = {
+          id: group.service_group_instance_id + '-s-' + service.service_id,
+          label: service.name,
+          tooltip: 'SERVICE: ' + service.service_id,
+          color: this.nalejColorScheme[this.nextColorIndex],
+          group: group.service_group_instance_id
+        };
+        this.graphData.nodes.push(nodeService);
+        this.graphData.links.push({
+          source: group.service_group_instance_id,
+          target: group.service_group_instance_id + '-s-' + service.service_id
+        });
+
+      });
+      this.nextColorIndex += 1;
+      if ( this.nextColorIndex >= this.nalejColorScheme.length ) {
+        this.nextColorIndex = 0;
+      }
+    });
+
+    if (instance.rules) {
+      instance.rules.forEach(rule => {
+        if (rule.auth_services) {
+          rule.auth_services.forEach(linkedService => {
+            console.log(linkedService);
+            const link = {
+              source: rule.target_service_group_name + rule.target_service_name,
+              target: linkedService
+            };
+            this.graphData.links.push(link);
+            console.log(link);
+          });
+        }
+      });
+    }
+  }
+
+  /**
+   * Transforms objects to arrays to be parsed to string and performed in the view
+   * @param object Key-value map that contains the object
+   */
+  objectToString(object: any) {
+    if (!object) {
+      return ['--'];
+    }
+    return Object.entries(object);
+  }
+
+  /**
+   * Updates instances array
+   * @param organizationId Organization identifier
+   */
+  updateAppInstances(organizationId: string) {
+    if (organizationId !== null) {
+      // Request to get apps instances
+      this.backend.getInstances(this.organizationId)
+      .subscribe(response => {
+        console.log('getinstances ', response);
+          this.instances = response.instances || [];
+          if (!this.loadedData) {
+            this.loadedData = true;
+          }
+      }, errorResponse => {
+        this.loadedData = true;
+        this.requestError = errorResponse.error.message;
+      });
+    }
+  }
+
+  /**
+   * Calculates the number of characters needed to hide the title of tabs, breakpoints calculated through manual testing
+   * @param event to pass in onResize method
+   */
+  @HostListener('window:resize', ['$event'])
+    onResize(event) {
+      if (event.target.innerWidth < 1280) {
+        this.maxLabelsLength = 55;
+      } else if (event.target.innerWidth < 1440) {
+        this.maxLabelsLength = 65;
+      } else if (event.target.innerWidth < 1613) {
+        this.maxLabelsLength = 75;
+      } else if (event.target.innerWidth < 1920) {
+        this.maxLabelsLength = 85;
+      } else {
+        this.maxLabelsLength = 100;
+      }
+    }
+
+    /**
+   * Parse to string labels map
+   * @param labels Key-value map that contains the labels
+   */
+  labelsToString(labels: any) {
+    if (!labels || labels === '-') {
+      return ;
+    }
+    return Object.entries(labels);
+  }
+
+   /**
+   * Opens the modal view that holds add label component
+   */
+  addLabel(entity) {
+    const initialState = {
+      organizationId: this.organizationId,
+      entityType: 'app',
+      entity: entity,
+      modalTitle: entity.name
+    };
+
+    this.modalRef = this.modalService.show(AddLabelComponent, {initialState, backdrop: 'static', ignoreBackdropClick: false });
+    this.modalRef.content.closeBtnName = 'Close';
+    this.modalService.onHide.subscribe((reason: string) => {
+      this.updateAppInstances(this.organizationId);
+    } );
+  }
+
+  /**
+   * Deletes a selected label
+   * @param entity selected label entity
+   */
+  deleteLabel(entity) {
+    // const deleteConfirm = confirm('Delete labels?');
+    // if (deleteConfirm) {
+    //   const index = this.selectedLabels.map(x => x.entityId).indexOf(entity.app_instance_id);
+    //   this.backend.updateAppInstances(
+    //     this.organizationId,
+    //     entity.app_instance_id,
+    //     {
+    //       organizationId: this.organizationId,
+    //       instanceId: entity.app_instance_id,
+    //       remove_labels: true,
+    //       labels: this.selectedLabels[index].labels
+    //     }).subscribe(updateAppResponse => {
+    //       this.selectedLabels.splice(index, 1);
+    //       this.updateAppInstances(this.organizationId);
+    //     });
+    // } else {
+    //   // Do nothing
+    // }
+  }
+
+  /**
+   * Selects a label
+   * @param entityId entity from selected label
+   * @param labelKey label key from selected label
+   * @param labelValue label value from selected label
+   */
+  onLabelClick(entityId, labelKey, labelValue) {
+    const selectedIndex = this.indexOfLabelSelected(entityId, labelKey, labelValue);
+    const newLabel = {
+      entityId: entityId,
+      labels: {}
+    } ;
+    if (selectedIndex === -1 ) {
+      const selected = this.selectedLabels.map(x => x.entityId).indexOf(entityId);
+      if (selected === -1) {
+        newLabel.labels[labelKey] = labelValue;
+        this.selectedLabels.push(newLabel);
+      } else {
+        this.selectedLabels[selected].labels[labelKey] = labelValue;
+      }
+    } else {
+      if (Object.keys(this.selectedLabels[selectedIndex].labels).length > 1) {
+        delete this.selectedLabels[selectedIndex].labels[labelKey];
+      } else {
+        this.selectedLabels.splice(selectedIndex, 1);
+      }
+    }
+  }
+
+  /**
+  * Check if the label is selected. Returs index number in selected labels or -1 if the label is not found.
+  * @param entityId entity from selected label
+  * @param labelKey label key from selected label
+  * @param labelValue label value from selected label
+  */
+  indexOfLabelSelected(entityId, labelKey, labelValue) {
+    for (let index = 0; index < this.selectedLabels.length; index++) {
+      if (this.selectedLabels[index].entityId === entityId &&
+          this.selectedLabels[index].labels[labelKey] === labelValue
+        ) {
+          return index;
+      }
+    }
+  return -1;
+  }
+  /**
+   * Check if any label is selected to change the state of add/delete buttons and to change class when a new label is about to be selected
+   * @param entityId entity from selected label
+   */
+  isAnyLabelSelected(entityId) {
+    if (this.selectedLabels.length > 0) {
+      const indexSelected = this.selectedLabels.map(x => x.entityId).indexOf(entityId);
+      if (indexSelected >= 0) {
+          return true;
+      }
+    }
+    return false;
+  }
+
+  /**
+   * Changes to active group
+   * @param groupId service group identifier
+   */
+  changeActiveGroup(groupId: string) {
+    this.activeGroupId = groupId;
+  }
+
+  /**
+   * Checks if the service group is active to show in the tabs
+   * @param groupId service group identifier
+   */
+  amIactive(groupId) {
+    if (groupId === this.activeGroupId) {
+      return 'active';
+    }
+    // Empty class when is not active
+    return '';
+  }
+
+  /**
+   * Checks if there are less than a maximum number groups in groups list
+   * @param groups groups list identifier
+   */
+  haveIGroups(groups) {
+    if (groups.length > this.DISPLAYED_GROUP_MAX) {
+      return '';
+    }
+    return 'opacity';
+  }
+
+  /**
+   * Sortby pipe in the component
+   * @param categoryName the name of the chosen category
+   */
+  setOrder(categoryName: string) {
+    if (this.sortedBy === categoryName) {
+      this.reverse = !this.reverse;
+      this.filterField = false;
+    }
+    this.sortedBy = categoryName;
+    this.filterField = true;
+  }
+
+  /**
+   * Reset all the filters fields
+   */
+  resetFilters() {
+    this.filterField = false;
+    this.searchTerm = '';
+    this.sortedBy = '';
+  }
+
+  /**
+   * Gets the category headers to add a class
+   * @param categoryName the class for the header category
+   */
+  getCategoryCSSClass(categoryName: string) {
+    if (this.sortedBy === '') {
+      return 'default';
+    } else {
+      if (this.sortedBy === categoryName) {
+        return 'enabled';
+      } else if (this.sortedBy !== categoryName) {
+        return 'disabled';
+      }
+    }
+  }
+
+  /**
+   * Updates the displayed groups chars length to calculate the number of letters displayed according to the size of the viewport
+   */
+  updateDisplayedGroupsNamesLength() {
+    this.displayedGroupsNamesLength = 0;
+    this.displayedGroups.forEach(group => {
+      this.displayedGroupsNamesLength += group.name.length;
+    });
+  }
+
+  /**
+   * Displayed groups list swipes left by pressing the arrow button functionality
+   */
+  swipeLeft() {
+    const index = this.groups.map(x => x.service_group_id).indexOf(this.displayedGroups[0].service_group_id);
+    if (index !== -1 && index > 0) {
+      this.displayedGroups.unshift(this.groups[index - 1]);
+      this.displayedGroups.pop();
+      this.updateDisplayedGroupsNamesLength();
+    }
+  }
+
+  /**
+   * Displayed groups list swipes right by pressing the arrow button functionality
+   */
+  swipeRight() {
+    const index = this.groups.map(x => x.service_group_id).indexOf(this.displayedGroups[this.displayedGroups.length - 1].service_group_id);
+    if (index !== -1 && this.groups[index + 1]) {
+      this.displayedGroups.push(this.groups[index + 1]);
+      this.displayedGroups.shift();
+    }
+    this.updateDisplayedGroupsNamesLength();
+  }
+
+   /**
+   * Requests an updated list of available services group to update the current one
+   * @param organizationId Organization identifier
+   */
+  updateGroupsList(organizationId: string) {
+    if (organizationId !== null) {
+      // Requests an updated services group list
+      this.backend.getGroups(this.organizationId)
+      .subscribe(response => {
+        this.groups = response.groups || [];
+        if (this.displayedGroups.length === 0 && this.groups.length > 0) {
+          for (let index = 0; index < this.groups.length && index < this.DISPLAYED_GROUP_MAX; index++) {
+            this.displayedGroups.push(this.groups[index]);
+          }
+        }
+        this.updateDisplayedGroupsNamesLength();
+        if (!this.loadedData) {
+          this.loadedData = true;
+        }
+      }, errorResponse => {
+          this.loadedData = true;
+          this.requestError = errorResponse.error.message;
+        });
+    }
+  }
+
+   /**
+   * Requests to undeploy the selected instance
+   * @param app Application instance object
+   */
+  undeploy(app) {
+    const undeployConfirm = confirm('Undeploy ' + app.name + '?');
+    if (undeployConfirm) {
+      this.backend.undeploy(this.organizationId, app.app_instance_id)
+        .subscribe(undeployResponse => {
+          this.notificationsService.add({
+            message: 'Undeploying ' + app.name,
+            timeout: 3000
+          });
+          this.updateAppInstances(this.organizationId);
+        }, error => {
+          this.notificationsService.add({
+            message: error.error.message,
+            timeout: 5000
+          });
+        });
+    }
+  }
+
+  /**
+   * Shows the graph in services card
+   */
+  showWindowGraph() {
+    this.showGraph = !this.showGraph;
+  }
+
+  /**
+   * Open serivces info modal window // TO BE DESIGNED
+   */
+  openServicesInfo() {
+
+  }
+
+  /**
+   * Open rules info modal window // TO BE DESIGNED
+   */
+  openRulesInfo() {
+
+  }
+
+
+
 
 }
