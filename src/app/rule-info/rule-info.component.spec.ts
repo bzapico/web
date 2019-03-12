05453e6a94951b5813fe2260baef8bca5db8ca77
Blanca <blanca@daisho.group>
2019-03-12 16:45:58 +0100

    Rule info styles created

--- src/app/rule-info/rule-info.component.spec.ts
+++ src/app/rule-info/rule-info.component.spec.ts
@@ -1,6 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RuleInfoComponent } from './rule-info.component';
+import { BsModalRef, ButtonsModule, BsModalService } from 'ngx-bootstrap';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { NgxGraphModule } from '@swimlane/ngx-graph';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { TooltipModule } from '@swimlane/ngx-charts';
 
 describe('RuleInfoComponent', () => {
   let component: RuleInfoComponent;
@@ -9,6 +15,19 @@ describe('RuleInfoComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ RuleInfoComponent ]
+      ,
+      imports: [
+        ButtonsModule,
+        HttpClientTestingModule,
+        BrowserAnimationsModule,
+        TooltipModule,
+        NgxGraphModule,
+        RouterTestingModule
+      ],
+      providers: [
+        BsModalRef,
+        BsModalService
+      ]
     })
     .compileComponents();
   }));
--- src/app/rule-info/rule-info.component.ts
+++ src/app/rule-info/rule-info.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Backend } from '../definitions/interfaces/backend';
+import { BsModalRef } from 'ngx-bootstrap';
+import { BackendService } from '../services/backend.service';
+import { MockupBackendService } from '../services/mockup-backend.service';
+import { LocalStorageKeys } from '../definitions/const/local-storage-keys';
+import { AppDescriptor } from '../definitions/interfaces/app-descriptor';
 
 @Component({
   selector: 'app-rule-info',
@@ -6,10 +12,84 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rule-info.component.scss']
 })
 export class RuleInfoComponent implements OnInit {
+  /**
+   * Backend reference
+   */
+  backend: Backend;
 
-  constructor() { }
+  /**
+   * Loaded Data status
+   */
+  loadedData: boolean;
+
+  /**
+   * Model that hold organization ID
+   */
+  organizationId: string;
+
+  /**
+   * Model that hold descriptor ID
+   */
+  descriptorId: string;
+
+  /**
+   * Model that hold registered application descriptor data
+   */
+  registeredData: AppDescriptor;
+
+  constructor(
+    public bsModalRef: BsModalRef,
+    private backendService: BackendService,
+    private mockupBackendService: MockupBackendService
+  ) {
+    const mock = localStorage.getItem(LocalStorageKeys.ruleInfoMock) || null;
+    // check which backend is required (fake or real)
+    if (mock && mock === 'true') {
+      this.backend = mockupBackendService;
+    } else {
+      this.backend = backendService;
+    }
+
+    // Default initialization
+    this.loadedData = true;
+   }
 
   ngOnInit() {
+    // if (this.organizationId !== null) {
+    //   this.backend.getAppDescriptor(this.organizationId, this.descriptorId)
+    //   .subscribe(registered => {
+    //     console.log(registered);
+    //     this.registeredData = registered;
+    //   });
+    // }
+
   }
+
+
+  /**
+   * Transforms objects to arrays to be parsed to string and performed in the view
+   * @param object Key-value map that contains the object
+   */
+  objectToString(object: any) {
+    if (!object) {
+      return ['--'];
+    }
+    return Object.entries(object);
+  }
+
+  /**
+   * Adds https in case of being required
+   * @param endpoint String containing the endpoint
+   */
+  getEndpointHref(endpoint: string) {
+    let URL = '';
+    if (!endpoint.startsWith('http') && !endpoint.startsWith('https')) {
+      URL = 'http://' + endpoint;
+    } else {
+      URL = endpoint;
+    }
+    return URL;
+  }
+
 
 }
