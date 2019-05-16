aa865709b130741f15327905e38d6f3203d66c74
Blanca <blanca@daisho.group>
2019-05-16 15:51:08 +0200

    Created contextual menu component

---
+++ src/app/contextual-menu/contextual-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contextual-menu',
+  templateUrl: './contextual-menu.component.html',
+  styleUrls: ['./contextual-menu.component.scss']
+})
+export class ContextualMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
