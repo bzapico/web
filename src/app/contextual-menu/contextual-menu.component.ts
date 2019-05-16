849332014380837af98336057709c417f750973f
Blanca <blanca@daisho.group>
2019-05-16 16:20:32 +0200

    Added functions to contextual menu

--- src/app/contextual-menu/contextual-menu.component.ts
+++ src/app/contextual-menu/contextual-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-contextual-menu',
@@ -8,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class ContextualMenuComponent implements OnInit {
 
   constructor() { }
+
+  @Input() visible: boolean;
+  options: string[];
 
   ngOnInit() {
   }
