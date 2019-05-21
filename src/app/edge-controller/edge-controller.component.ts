9e825d2278b05fcbf71751f36cab1be5a24f8aed
Blanca <blanca@daisho.group>
2019-05-21 15:30:22 +0200

    EC layout

--- src/app/edge-controller/edge-controller.component.ts
+++ src/app/edge-controller/edge-controller.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Backend } from '../definitions/interfaces/backend';
+import { BsModalRef } from 'ngx-bootstrap';
+import { BackendService } from '../services/backend.service';
+import { MockupBackendService } from '../services/mockup-backend.service';
+import { LocalStorageKeys } from '../definitions/const/local-storage-keys';
 
 @Component({
   selector: 'app-edge-controller',
@@ -6,10 +11,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edge-controller.component.scss']
 })
 export class EdgeControllerComponent implements OnInit {
+   /**
+   * Backend reference
+   */
+  backend: Backend;
 
-  constructor() { }
+  controllerId: string;
+  show: string;
+  created: string;
+  name: string;
+  labels: any;
+  status: string;
+
+  /**
+   * Loaded Data status
+   */
+  loadedData: boolean;
+
+  constructor(
+    public bsModalRef: BsModalRef,
+    private backendService: BackendService,
+    private mockupBackendService: MockupBackendService
+  ) {
+    const mock = localStorage.getItem(LocalStorageKeys.serviceInstancesInfoMock) || null;
+    // check which backend is required (fake or real)
+    if (mock && mock === 'true') {
+      this.backend = mockupBackendService;
+    } else {
+      this.backend = backendService;
+    }
+
+    // Default initialization
+    this.loadedData = true;
+   }
 
   ngOnInit() {
   }
 
+  /**
+   * Transforms objects to arrays to be parsed to string and performed in the view
+   * @param object Key-value map that contains the object
+   */
+  objectToString(object: any) {
+    if (!object) {
+      return ['--'];
+    }
+    return Object.entries(object);
+  }
 }
