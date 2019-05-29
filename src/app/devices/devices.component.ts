96c534241ef385f81ed6bacdc9a8151847ec1e67
Blanca <blanca@daisho.group>
2019-05-29 16:22:37 +0200

    Added activated rout to devices controller to be opened from devices info in infrastructure view

--- src/app/devices/devices.component.ts
+++ src/app/devices/devices.component.ts
@@ -9,6 +9,7 @@ import { mockDevicesChart } from '../utils/mocks';
 import { AddDevicesGroupComponent } from '../add-devices-group/add-devices-group.component';
 import { GroupConfigurationComponent } from '../group-configuration/group-configuration.component';
 import { AddLabelComponent } from '../add-label/add-label.component';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-devices',
@@ -65,7 +66,6 @@ export class DevicesComponent implements OnInit, OnDestroy  {
    * List of active displayed group
    */
   displayedGroups: any[];
-
   devicesOnTimeline: any[];
 
   /**
@@ -163,6 +163,8 @@ export class DevicesComponent implements OnInit, OnDestroy  {
     private backendService: BackendService,
     private mockupBackendService: MockupBackendService,
     private notificationsService: NotificationsService,
+    private router: Router,
+    private route: ActivatedRoute,
   ) {
     const mock = localStorage.getItem(LocalStorageKeys.devicesMock) || null;
     // Check which backend is required (fake or real)
@@ -204,6 +206,9 @@ export class DevicesComponent implements OnInit, OnDestroy  {
   }
 
   ngOnInit() {
+    if (this.route.snapshot.queryParamMap.get('groupId')) {
+      this.activeGroupId = this.route.snapshot.queryParamMap.get('groupId');
+    }
     // Get User data from localStorage
     const jwtData = localStorage.getItem(LocalStorageKeys.jwtData) || null;
     if (jwtData !== null) {
