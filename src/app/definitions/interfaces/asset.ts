f1a69c06a15fbd880f0eef6283d375fabcc79ec6
Blanca <blanca@daisho.group>
2019-07-26 12:09:21 +0300

    Updated interfaces definitios

--- src/app/definitions/interfaces/asset.ts
+++ src/app/definitions/interfaces/asset.ts
@@ -11,8 +11,6 @@ export interface Asset {
     asset_id: string;
     // AgentId with the agent identifier that is monitoring this asset if any.
     agent_id: string;
-    // EicNetIp contains the current IP address that connects the asset to the EIC.
-    eic_net_ip: string;
     // Show flag to determine if this asset should be shown on the UI. This flag is internally used
     // for the async uninstall/removal of the asset.
     show?: boolean;
@@ -20,18 +18,22 @@ export interface Asset {
     created?: number;
     // Labels defined by the user.
     labels?: any;
-    // Edge Controller name
-    // name?: string;
     // OS contains Operating System information.
     os?: any;
     // Hardware information.
     hardware?: any;
     // Storage information.
     storage?: any;
+    // EicNetIp contains the current IP address that connects the asset to the EIC.
+    eic_net_ip: string;
     // LastOpSummary contains the result of the last operation fr this asset
     last_op_summary?: any;
     // LastAliveTimestamp contains the last alive message received
     last_alive_timestamp?: string;
     // Status of the agent.
     status?: string;
+    // StatusName with the status of the asset.
+    status_name?: string;
+    // location with the asset location
+    location?: string;
 }
--- src/app/definitions/interfaces/device.ts
+++ src/app/definitions/interfaces/device.ts
@@ -3,11 +3,13 @@
  */
 export interface Device {
   // OrganizationId with the organization identifier.
-  organization_id?: string;
+  organization_id: string;
   // DevicesGroupId with the device group identifier.
-  device_group_id?: string;
+  device_group_id: string;
   // DeviceId with the device identifier.
-  device_id?: string;
+  device_id: string;
+  // AssetDeviceId is the composite of DeviceID and DeviceGroupId
+  asset_device_id?: string;
   // RegisterSince is the timestamp when the device joined the group.
   register_since?: number;
   // Labels defined by the user.
@@ -16,6 +18,10 @@ export interface Device {
   enabled?: boolean;
   // DeviceApiKey contains the API KEY used by the device to send data.
   device_api_key?: string;
-  // Status of device.
+  // DeviceStatus contains the status of the device (ONLINE/OFFLINE)
   device_status_name?: string;
+  // location with the device location
+  location?: any;
+  // AssetInfo with the information related to Hw, Storage and OS
+  asset_info?: any;
 }
