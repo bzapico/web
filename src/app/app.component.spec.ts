f5a6c3b6e4a1fbb7cd50f8840df367c271640205
Jonathan Rodríguez <jrodriguez@nalej.com>
2019-09-10 18:07:44 +0200

    Fixing all testing errors

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,7 +13,7 @@ import { AlertModule } from 'ngx-bootstrap/alert';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { SortByPipe } from './pipes/sort-by.pipe';
 import { FilterPipe } from './pipes/filter.pipe';
-import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+import {TranslateLoader, TranslateModule, TranslateService} from '@ngx-translate/core';
 import { createTranslateLoader } from './app.module';
 import { HttpClient } from '@angular/common/http';
 
