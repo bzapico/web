c2dcc5290f494a553f43d1191bcafe1d6d4ec26d
Blanca Zapico <blanca@nalej.com>
2019-09-12 12:45:54 +0200

    Styled component

--- src/app/advanced-filter-options/advanced-filter-options.component.spec.ts
+++ src/app/advanced-filter-options/advanced-filter-options.component.spec.ts
@@ -1,6 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AdvancedFilterOptionsComponent } from './advanced-filter-options.component';
+import { FormsModule } from '@angular/forms';
+import { ButtonsModule, BsModalRef, TooltipModule, ModalModule, BsModalService } from 'ngx-bootstrap';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { By } from '@angular/platform-browser';
 
 describe('AdvancedFilterOptionsComponent', () => {
   let component: AdvancedFilterOptionsComponent;
@@ -8,7 +13,19 @@ describe('AdvancedFilterOptionsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AdvancedFilterOptionsComponent ]
+      declarations: [ AdvancedFilterOptionsComponent ],
+      imports: [
+        FormsModule,
+        ButtonsModule,
+        HttpClientTestingModule,
+        TooltipModule,
+        RouterTestingModule,
+        ModalModule.forRoot(),
+      ],
+      providers: [
+        BsModalRef,
+        BsModalService
+      ]
     })
     .compileComponents();
   }));
@@ -19,7 +36,19 @@ describe('AdvancedFilterOptionsComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('saveFilterChanges() - Should request to save the group data modifications', () => {
+    const saveFilterChanges = spyOn(component, 'saveFilterChanges').and.returnValue(true);
+
+    saveFilterChanges();
+
+    expect(saveFilterChanges).toHaveBeenCalledWith();
+  });
+
+  it('Discard button - Should discard the group data modifications', () => {
+    const button = fixture.debugElement.query(By.css('.close'));
+
+    button.triggerEventHandler('click', null);
+
+    expect(component.closeModal).toBeDefined();
   });
 });
--- src/app/advanced-filter-options/advanced-filter-options.component.ts
+++ src/app/advanced-filter-options/advanced-filter-options.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Backend } from '../definitions/interfaces/backend';
+import { BsModalRef } from 'ngx-bootstrap';
+import { BackendService } from '../services/backend.service';
+import { MockupBackendService } from '../services/mockup-backend.service';
+import { LocalStorageKeys } from '../definitions/const/local-storage-keys';
 
 @Component({
   selector: 'advanced-filter-options',
@@ -6,10 +11,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./advanced-filter-options.component.scss']
 })
 export class AdvancedFilterOptionsComponent implements OnInit {
+  /**
+   * Backend reference
+   */
+  backend: Backend;
 
-  constructor() { }
+  /**
+   * Model that hold organization ID, default filter, enabled or disabled option
+   */
+  organizationId: string;
+  defaultFilter: boolean;
+  enabled: boolean;
+  name: string;
+
+  /**
+   * Models that removes the possibility for the user to close the modal by clicking outside the content card
+   */
+  config = {
+    backdrop: false,
+    ignoreBackdropClick: true
+  };
+
+  constructor(
+    public bsModalRef: BsModalRef,
+    private backendService: BackendService,
+    private mockupBackendService: MockupBackendService,
+  ) {
+    const mock = localStorage.getItem(LocalStorageKeys.advancedFilterOptions) || null;
+    // check which backend is required (fake or real)
+    if (mock && mock === 'true') {
+      this.backend = this.mockupBackendService;
+    } else {
+      this.backend = this.backendService;
+    }
+    // Default initialization
+    this.defaultFilter = false;
+    this.enabled = false;
+  }
 
   ngOnInit() {
+  }
+
+  /**
+   *  Request to save the advanced filter modifications
+   */
+  saveFilterChanges() {
+    this.bsModalRef.hide();
+  }
+
+  /**
+   * Close the modal window
+   */
+  closeModal() {
+    this.bsModalRef.hide();
   }
 
 }
