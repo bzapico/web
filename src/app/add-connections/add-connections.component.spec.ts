40176e3287cf69004ba8fe8cc4a5f0ea2fcddaeb
Blanca Zapico <blanca@nalej.com>
2019-09-23 11:19:36 +0200

    Created structure for add connections

--- src/app/add-connections/add-connections.component.spec.ts
+++ src/app/add-connections/add-connections.component.spec.ts
@@ -1,6 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AddConnectionsComponent } from './add-connections.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ButtonsModule, BsModalRef, TooltipModule } from 'ngx-bootstrap';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { SelectDropDownModule } from 'ngx-select-dropdown';
 
 describe('AddConnectionsComponent', () => {
   let component: AddConnectionsComponent;
@@ -8,7 +13,19 @@ describe('AddConnectionsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AddConnectionsComponent ]
+      declarations: [ AddConnectionsComponent ],
+      imports: [
+        FormsModule,
+        ButtonsModule,
+        HttpClientTestingModule,
+        TooltipModule,
+        SelectDropDownModule,
+        RouterTestingModule,
+        ReactiveFormsModule
+      ],
+      providers: [
+        BsModalRef,
+      ]
     })
     .compileComponents();
   }));
--- src/app/add-connections/add-connections.component.ts
+++ src/app/add-connections/add-connections.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Backend } from '../definitions/interfaces/backend';
 import { BsModalRef } from 'ngx-bootstrap';
+import { BackendService } from '../services/backend.service';
+import { MockupBackendService } from '../services/mockup-backend.service';
+import { LocalStorageKeys } from '../definitions/const/local-storage-keys';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'add-connections',
@@ -8,20 +13,200 @@ import { BsModalRef } from 'ngx-bootstrap';
 })
 export class AddConnectionsComponent implements OnInit {
   /**
-   * Model that hold manage connections and its info
+   * Backend reference
+   */
+  backend: Backend;
+
+  /**
+   * Models that holds forms info
+   */
+  addNewConnectionsForm: FormGroup;
+  submitted = false;
+  loading: boolean;
+
+  /**
+   * Model that hold add connections and its info
    */
   organizationId: string;
+  sourceInstances: any[];
+  sourceInterfaces: any[];
+  targetInterfaces: any[];
+  targetInstances: any[];
+  sourceInstance: FormControl;
+  sourceInterface: FormControl;
+  targetInstance: FormControl;
+  targetInterface: FormControl;
 
-  constructor( public bsModalRef: BsModalRef) { }
+  /**
+   * NGX-select-dropdown
+   */
+  tab = 1;
+  options = [];
+  selectConfig = {};
+  defaultAutofocus: string;
+
+  sourceInstanceOptions: any[];
+  sourceInstanceConfig = {};
+
+  sourceInterfaceOptions: any[];
+  sourceInterfaceConfig = {};
+
+  targetInstanceOptions: any[];
+  targetInstanceConfig = {};
+
+  targetInterfaceOptions: any[];
+  targetInterfaceConfig = {};
+
+  constructor(
+    private formBuilder: FormBuilder,
+    public bsModalRef: BsModalRef,
+    private backendService: BackendService,
+    private mockupBackendService: MockupBackendService,
+    ) {
+      const mock = localStorage.getItem(LocalStorageKeys.addConnectionsMock) || null;
+      // check which backend is required (fake or real)
+      if (mock && mock === 'true') {
+        this.backend = this.mockupBackendService;
+      } else {
+        this.backend = this.backendService;
+      }
+    //  Source Instance
+    this.sourceInstances = [];
+    this.sourceInstanceOptions = [{
+      name: 'App1',
+      code: 0
+    }, {
+      name: 'App2',
+      code: 1
+    }, {
+      name: 'App3',
+      code: 2
+    }, {
+      name: 'App4',
+      code: 3
+    }];
+    //  Source Interfaces
+    this.sourceInterfaces = [];
+    this.sourceInterfaceOptions = [{
+      name: 'App1',
+      code: 0
+    }, {
+      name: 'App2',
+      code: 1
+    }, {
+      name: 'App3',
+      code: 2
+    }, {
+      name: 'App4',
+      code: 3
+    }];
+    //  Target Interfaces
+    this.targetInterfaces = [];
+    this.targetInterfaceOptions = [{
+      name: 'App1',
+      code: 0
+    }, {
+      name: 'App2',
+      code: 1
+    }, {
+      name: 'App3',
+      code: 2
+    }, {
+      name: 'App4',
+      code: 3
+    }];
+    //  Target Instances
+    this.targetInstances = [];
+    this.targetInstanceOptions = [{
+      name: 'App1',
+      code: 0
+    }, {
+      name: 'App2',
+      code: 1
+    }, {
+      name: 'App3',
+      code: 2
+    }, {
+      name: 'App4',
+      code: 3
+    }];
+
+    }
 
   ngOnInit() {
+    this.addNewConnectionsForm = this.formBuilder.group({
+      sourceInstance: [null, Validators.required],
+      sourceInterface: [null, Validators.required],
+      targetInstance: [null, Validators.required],
+      targetInterface: [null, Validators.required],
+    });
+    this.sourceInstanceConfig = {
+      displayKey: 'name',
+      search: false,
+      height: 'auto',
+      placeholder: 'Source Instance',
+      limitTo: 4,
+      moreText: 'more',
+      noResultsFound: 'No results found!'
+    };
+    this.sourceInterfaceConfig = {
+      displayKey: 'name',
+      search: false,
+      height: 'auto',
+      placeholder: 'Source Interface',
+      limitTo: 4,
+      moreText: 'more',
+      noResultsFound: 'No results found!'
+    };
+    this.targetInstanceConfig = {
+      displayKey: 'name',
+      search: false,
+      height: 'auto',
+      placeholder: 'Target Instance',
+      limitTo: 4,
+      moreText: 'more',
+      noResultsFound: 'No results found!'
+    };
+    this.targetInterfaceConfig = {
+      displayKey: 'name',
+      search: false,
+      height: 'auto',
+      placeholder: 'Target Interface',
+      limitTo: 4,
+      moreText: 'more',
+      noResultsFound: 'No results found!'
+    };
   }
 
   /**
-   * Close the modal window
+   * Convenience getter for easy access to form fields
    */
-  closeModal() {
-    this.bsModalRef.hide();
+  get f() { return this.addNewConnectionsForm.controls; }
+
+  /**
+   * Requests to add new connections
+   * @param f Form with the connections input data
+   */
+  addNewConnections(f) {
+    this.submitted = true;
+    this.loading = true;
   }
 
+  /**
+   * Checks if the form has been modified before discarding changes
+   * @param form Form object reference
+   */
+  discardChanges(form) {
+    if (form.dirty) {
+      const discard = confirm('Discard changes?');
+      if (discard) {
+        this.bsModalRef.hide();
+      } else {
+        // Do nothing
+      }
+    } else {
+      this.bsModalRef.hide();
+    }
+  }
 }
+
