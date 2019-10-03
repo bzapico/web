cbb6f6cbaa8071bc27c5e6059df125abddb7d7fe
Blanca Zapico <blanca@nalej.com>
2019-10-03 09:12:24 +0200

    Added logic to labels card component

--- src/app/labels-card/labels-card.component.spec.ts
+++ src/app/labels-card/labels-card.component.spec.ts
@@ -1,6 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LabelsCardComponent } from './labels-card.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TooltipModule } from '@swimlane/ngx-charts';
+import { ButtonsModule, ModalModule, BsModalRef, BsModalService } from 'ngx-bootstrap';
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
+import { createTranslateLoader } from '../app.module';
 
 describe('LabelsCardComponent', () => {
   let component: LabelsCardComponent;
@@ -8,7 +14,25 @@ describe('LabelsCardComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LabelsCardComponent ]
+      declarations: [ LabelsCardComponent ],
+    imports: [
+      HttpClientTestingModule,
+      TooltipModule,
+      ButtonsModule,
+      ModalModule.forRoot(),
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: (createTranslateLoader),
+          deps: [HttpClient]
+        }
+      })
+    ],
+    providers: [
+      BsModalRef,
+      BsModalService,
+      TranslateService
+    ]
     })
     .compileComponents();
   }));
--- src/app/labels-card/labels-card.component.ts
+++ src/app/labels-card/labels-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { LabelsCardServiceService } from '../labels-card-service.service';
 
 @Component({
   selector: 'labels-card',
@@ -6,10 +7,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./labels-card.component.scss']
 })
 export class LabelsCardComponent implements OnInit {
+  @Input() labelsData: any[];
+  @Input() selectableLabel: boolean;
 
-  constructor() { }
+  selectedLabels: any[];
 
-  ngOnInit() {
+  constructor(
+    private labelsCardService: LabelsCardServiceService
+  ) {
+    this.selectedLabels = [];
   }
 
+  ngOnInit() {}
+
+  /**
+   * Opens the modal view that holds add label component
+   * @param entity selected label entity
+   */
+  addLabel(labelsData) {
+    this.labelsCardService.addLabel(labelsData);
+  }
+
+  /**
+   * Deletes a selected label
+   * @param entity selected label entity
+   * @param selectedLabels selected labels array
+   */
+  deleteLabel(labelsData, selectedLabels) {
+    this.labelsCardService.deleteLabel(labelsData, selectedLabels);
+  }
+
+  /**
+   * Selects a label
+   * @param label label entity
+   * @param selectedLabels selected labels array
+   */
+  onLabelClick(label, selectedLabels) {
+    this.labelsCardService.onLabelClick(label, selectedLabels);
+  }
 }
