19847c993b58fa11e559637b333d42453c5728a7
Blanca Zapico <blanca@nalej.com>
2019-10-05 11:05:14 +0200

    Added styles for instances in app-info-detailed component

--- src/app/app-info-detailed/app-info-detailed.component.spec.ts
+++ src/app/app-info-detailed/app-info-detailed.component.spec.ts
@@ -1,6 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AppInfoDetailedComponent } from './app-info-detailed.component';
+import { BsModalRef, ButtonsModule, BsModalService, TooltipModule } from 'ngx-bootstrap';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
+import { createTranslateLoader } from '../app.module';
 
 describe('AppInfoDetailedComponent', () => {
   let component: AppInfoDetailedComponent;
@@ -8,7 +15,26 @@ describe('AppInfoDetailedComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AppInfoDetailedComponent ]
+      declarations: [ AppInfoDetailedComponent ],
+      imports: [
+        ButtonsModule,
+        HttpClientTestingModule,
+        BrowserAnimationsModule,
+        TooltipModule,
+        RouterTestingModule,
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useFactory: (createTranslateLoader),
+            deps: [HttpClient]
+          }
+        })
+      ],
+      providers: [
+        BsModalRef,
+        BsModalService,
+        TranslateService
+      ]
     })
     .compileComponents();
   }));
--- src/app/app-info-detailed/app-info-detailed.component.ts
+++ src/app/app-info-detailed/app-info-detailed.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BsModalRef } from 'ngx-bootstrap';
 
 @Component({
   selector: 'app-info-detailed',
@@ -6,10 +7,102 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app-info-detailed.component.scss']
 })
 export class AppInfoDetailedComponent implements OnInit {
+  /**
+   * Data model to show where the modal is triggered
+   */
+  openFromRegistered: boolean;
 
-  constructor() { }
+  /**
+   * Data models for app related information
+   */
+  organizationId: string;
+  appDescriptorId: string;
+  appInstanceId: string;
+  name: string;
+  group: any;
+  statusName: string;
+  labels: any;
+
+  /**
+   * Loaded Data status
+   */
+  loadedData: boolean;
+
+  constructor(
+    public bsModalRef: BsModalRef,
+  ) {
+    this.openFromRegistered = false;
+  }
 
   ngOnInit() {
+
+    console.log('las labels ', this.labels);
+  }
+
+  /**
+   * Adds https in case of being required
+   * @param endpoint String containing the endpoint
+   */
+  getEndpointHref(endpoint: string) {
+    let URL = '';
+    if (!endpoint.startsWith('http') && !endpoint.startsWith('https')) {
+      URL = 'http://' + endpoint;
+    } else {
+      URL = endpoint;
+    }
+    return URL;
+  }
+
+  /**
+   * Checks if the service status requires an special css class
+   * @param status Service status name
+   * @param className CSS class name
+   */
+  classStatusCheck(status: string, className: string): boolean {
+    switch (status.toLowerCase()) {
+      case 'service_running': {
+        if (className.toLowerCase() === 'service_running') {
+          return true;
+        }
+        break;
+      }
+      case 'service_error': {
+        if (className.toLowerCase() === 'service_error') {
+          return true;
+        }
+        break;
+      }
+      case 'service_waiting': {
+        if (className.toLowerCase() === 'service_waiting') {
+          return true;
+        }
+        break;
+      }
+      default: {
+        if (className.toLowerCase() === 'service_scheduled') {
+          return true;
+        }
+        return false;
+      }
+    }
+  }
+
+  /**
+   * Filters the backend incoming status to display it in removing the initial "service_"
+   * @param rawStatus string containing the status that the backend is sending
+   */
+  getBeautyStatusName (rawStatus: string): string {
+    if (rawStatus.toLowerCase().startsWith('service_')) {
+      return rawStatus.substring('service_'.length, rawStatus.length);
+    }
+    return rawStatus;
+  }
+
+    /**
+   * Close the modal window
+   */
+  closeModal() {
+    this.bsModalRef.hide();
   }
 
 }
