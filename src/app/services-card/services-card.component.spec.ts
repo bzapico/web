188cac878c7089e6a9a0f4124b8d319ffc80bc34
Blanca Zapico <blanca@nalej.com>
2019-10-10 11:23:25 +0200

    Added styles, logic and test

--- src/app/services-card/services-card.component.spec.ts
+++ src/app/services-card/services-card.component.spec.ts
@@ -1,6 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ServicesCardComponent } from './services-card.component';
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
+import { createTranslateLoader } from '../app.module';
+import { TooltipModule, ModalModule } from 'ngx-bootstrap';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('ServicesCardComponent', () => {
   let component: ServicesCardComponent;
@@ -8,7 +14,23 @@ describe('ServicesCardComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ServicesCardComponent ]
+      declarations: [ ServicesCardComponent ],
+      imports: [
+        TooltipModule.forRoot(),
+        HttpClientTestingModule,
+        RouterTestingModule,
+        ModalModule.forRoot(),
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useFactory: (createTranslateLoader),
+            deps: [HttpClient]
+          }
+        })
+      ],
+      providers: [
+        TranslateService
+      ]
     })
     .compileComponents();
   }));
--- src/app/services-card/services-card.component.ts
+++ src/app/services-card/services-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'services-card',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./services-card.component.scss']
 })
 export class ServicesCardComponent implements OnInit {
+  /**
+   *  Show the services graph tab
+   */
+  showGraph: boolean;
 
-  constructor() { }
+  constructor(   private translateService: TranslateService) {
+    this.showGraph = true;
+  }
 
   ngOnInit() {
+  }
+
+  /**
+   * Shows the graph in services card
+   */
+  selectDisplayMode(type: string) {
+    if (type === this.translateService.instant('graph.typeList')) {
+      this.showGraph = false;
+    } else if (type === this.translateService.instant('graph.typeGraph')) {
+      this.showGraph = true;
+    }
   }
 
 }
