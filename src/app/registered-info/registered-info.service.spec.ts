13ae68d1cdb3140cce97c4b455381ec50237c5c3
Jonathan Rodríguez <jrodriguez@nalej.com>
2019-10-10 10:37:11 +0200

    Fixing test issue

--- src/app/registered-info/registered-info.service.spec.ts
+++ src/app/registered-info/registered-info.service.spec.ts
@@ -1,9 +1,26 @@
 import { TestBed } from '@angular/core/testing';
-
 import { RegisteredInfoService } from './registered-info.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+import { createTranslateLoader } from '../app.module';
+import { HttpClient } from '@angular/common/http';
 
-describe('RegisteredInfoService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+describe('InstanceInfoService', () => {
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      HttpClientTestingModule,
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: (createTranslateLoader),
+          deps: [HttpClient]
+        }
+      })
+    ],
+    providers: [
+      TranslateService
+    ]
+  }));
 
   it('should be created', () => {
     const service: RegisteredInfoService = TestBed.get(RegisteredInfoService);
