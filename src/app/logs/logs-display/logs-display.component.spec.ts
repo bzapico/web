cc9f2f4c5d9f685ff8568f1fe48bf305112508e6
Blanca Zapico <blanca@nalej.com>
2019-12-11 10:55:50 +0100

    Added date pipe to show formatted data

--- src/app/logs/logs-display/logs-display.component.spec.ts
+++ src/app/logs/logs-display/logs-display.component.spec.ts
@@ -1,6 +1,23 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+/*
+ *  Copyright 2019 Nalej
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { LogsDisplayComponent } from './logs-display.component';
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
+import { createTranslateLoader } from 'src/app/app.module';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('LogsDisplayComponent', () => {
   let component: LogsDisplayComponent;
@@ -8,7 +25,23 @@ describe('LogsDisplayComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LogsDisplayComponent ]
+      declarations: [
+        LogsDisplayComponent
+    ],
+    imports: [
+      HttpClientTestingModule,
+      RouterTestingModule,
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: (createTranslateLoader),
+          deps: [HttpClient]
+        }
+      }),
+    ],
+    providers: [
+      TranslateService
+    ]
     })
     .compileComponents();
   }));
