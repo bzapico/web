d175bca25e9d1e4998d6c6e4dbf3d30eca2147ae
Blanca Zapico <blanca@nalej.com>
2020-01-21 13:02:53 +0100

    Fixed tests

--- src/app/logs/logs.service.spec.ts
+++ src/app/logs/logs.service.spec.ts
@@ -13,9 +13,27 @@
 
 import { TestBed } from '@angular/core/testing';
 import { LogsService } from './logs.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+import { createTranslateLoader } from '../app.module';
+import { HttpClient } from '@angular/common/http';
 
 describe('LogsService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      HttpClientTestingModule,
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: (createTranslateLoader),
+          deps: [HttpClient]
+        }
+      })
+    ],
+    providers: [
+      TranslateService
+    ]
+  }));
 
   it('should be created', () => {
     const service: LogsService = TestBed.get(LogsService);
