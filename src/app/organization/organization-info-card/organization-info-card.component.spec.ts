31f69aa875f7b7f7250f3e8af12c2f924c109884
Blanca Zapico <blanca@nalej.com>
2020-01-27 14:16:15 +0100

    Organization info card component created

---
+++ src/app/organization/organization-info-card/organization-info-card.component.spec.ts
@@ -0,0 +1,57 @@
+/*
+ *  Copyright 2019 Nalej
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { OrganizationInfoCardComponent } from './organization-info-card.component';
+import { ButtonsModule, BsModalRef, TooltipModule } from 'ngx-bootstrap';
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
+import { createTranslateLoader } from '../../app.module';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
+describe('OrganizationInfoCardComponent', () => {
+  let component: OrganizationInfoCardComponent;
+  let fixture: ComponentFixture<OrganizationInfoCardComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ OrganizationInfoCardComponent ],
+      imports: [
+        ButtonsModule,
+        TooltipModule.forRoot(),
+        HttpClientTestingModule,
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useFactory: (createTranslateLoader),
+            deps: [HttpClient]
+          }
+        })
+      ],
+      providers: [
+        BsModalRef,
+        TranslateService
+      ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(OrganizationInfoCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
